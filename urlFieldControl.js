66d57a8c6829babea10d50a5ac791f1512840dc8
MrBabbelFish <120329158+MrBabbelFish@users.noreply.github.com>
2023-01-26 10:07:31 +0000

    Introduced more fields to be used in the Url

--- urlFieldControl.js
+++ urlFieldControl.js
@@ -6,15 +6,25 @@ function urlFieldUpdate (workItemServices) {
     workItemServices.WorkItemFormService.getService().then(function (service) {
         var url = VSS.getConfiguration().witInputs.Url;
         var titleUrlText = VSS.getConfiguration().witInputs.Title;
+
         var fieldName = VSS.getConfiguration().witInputs.Field;
-        var hideUrlIfEmptyField = (VSS.getConfiguration().witInputs.HideUrlIfEmptyField.toLowerCase() === 'true');
+        var fieldName2 = VSS.getConfiguration().witInputs.Field2;
+        var fieldName3 = VSS.getConfiguration().witInputs.Field3;
         
-        service.getFieldValues([VSS.getConfiguration().witInputs.Field]).then(function (value) {
+        var hideUrlIfEmptyField = (VSS.getConfiguration().witInputs.HideUrlIfEmptyField.toLowerCase() === 'true');
+
+        service.getFieldValues([fieldName, fieldName2, fieldName3]).then(function (value) {
             var fieldValue = value[fieldName];
+            var fieldValue2 = value[fieldName2];
+            var fieldValue3 = value[fieldName3];
+
             url = url.replace('{field}', fieldValue);
             if (isEmpty(titleUrlText)) {
                 titleUrlText = url;
             }
+            url = url.replace('{field2}', fieldValue2);
+            url = url.replace('{field3}', fieldValue3);
+
             var urlField = document.getElementById('urlField');
             if (hideUrlIfEmptyField && isEmpty(fieldValue)) {
                 urlField.innerText = "";
